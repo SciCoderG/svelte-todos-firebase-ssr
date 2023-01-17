a3db05fe313776139a11e4de94f252b219fe4632
David Liebemann <david@crystalmesh.de>
2023-01-17 18:55:35 +0100

    Basic project

---
+++ public/_app/immutable/chunks/1-f8bd6438.js
@@ -0,0 +1,1 @@
+import{default as t}from"../components/error.svelte-261f006a.js";export{t as component};
---
+++ public/_app/immutable/chunks/2-841b4452.js
@@ -0,0 +1,1 @@
+import{default as t}from"../components/pages/_page.svelte-baedb771.js";export{t as component};
